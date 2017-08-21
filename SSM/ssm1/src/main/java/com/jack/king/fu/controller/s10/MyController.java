b2b373355f7420a10e103b72bf0330e87b08f1fe
JacKing <176288735@qq.com>
2017-08-21 18:29:52 +0800

    parameterMethodNameResolver 方法解析器

---
+++ SSM/ssm1/src/main/java/com/jack/king/fu/controller/s10/MyController.java
@@ -0,0 +1,28 @@
+package com.jack.king.fu.controller.s10;
+
+import org.apache.logging.log4j.LogManager;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyController extends MultiActionController {
+
+    public static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+
+    public ModelAndView doFirst(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        //其底层调用的是request.setAttribute()方法
+        mv.addObject("message", "propertiesMethodNameResolver doFirst");
+        mv.setViewName("/WEB-INF/Jsp/welcome.jsp");
+        return mv;
+    }
+    public ModelAndView doSecond(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        //其底层调用的是request.setAttribute()方法
+        mv.addObject("message", "propertiesMethodNameResolver doSecond");
+        mv.setViewName("/WEB-INF/Jsp/welcome.jsp");
+        return mv;
+    }
+}
