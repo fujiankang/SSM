6e521f7dfe37ee3a8cca4f5b520b58cbc1d4671c
JacKing <176288735@qq.com>
2017-08-21 18:27:20 +0800

    请求一个Controller的多个方法

---
+++ SSM/ssm1/src/main/java/com/jack/king/fu/controller/s8/MyController.java
@@ -0,0 +1,26 @@
+package com.jack.king.fu.controller.s8;
+import org.apache.logging.log4j.LogManager;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyController extends MultiActionController {
+
+    public static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+
+    public ModelAndView doFirst(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        //其底层调用的是request.setAttribute()方法
+        mv.addObject("message", "HelloWorld doFirst");
+        mv.setViewName("/WEB-INF/Jsp/welcome.jsp");
+        return mv;
+    }
+    public ModelAndView doSecond(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        //其底层调用的是request.setAttribute()方法
+        mv.addObject("message", "HelloWorld doSecond");
+        mv.setViewName("/WEB-INF/Jsp/welcome.jsp");
+        return mv;
+    }
+}
