dda638ebd3699fa10d3a8c9884eb7be2be25d054
JacKing <176288735@qq.com>
2017-08-22 14:59:03 +0800

    XmlViewResolver,XML的视图解析器

---
+++ SSM/ssm1/src/main/java/com/jack/king/fu/controller/s12/MyController.java
@@ -0,0 +1,19 @@
+package com.jack.king.fu.controller.s12;
+
+import org.apache.logging.log4j.LogManager;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyController implements Controller {
+    //Bean名称视图解析器
+    public static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+       logger.error("测试XmlViewResolver");
+        return new ModelAndView("jd");
+    }
+}
