09a2b00dc62ba70eb2a025a9970d561200a98210
JacKing <176288735@qq.com>
2017-08-22 18:36:19 +0800

    命名空间的使用

---
+++ SSM/ssm1/src/main/java/com/jack/king/fu/annotations/a3/annotationController.java
@@ -0,0 +1,26 @@
+package com.jack.king.fu.annotations.a3;
+
+import org.apache.logging.log4j.LogManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/king/love") //命名空间
+public class annotationController {
+    public static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+
+    @RequestMapping({"/my.do","hello.do"})
+    public ModelAndView doFirst(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        logger.info("一个控制器多个访问路径my.do,hello.do");
+        return new ModelAndView("/WEB-INF/Jsp/welcome.jsp");
+    }
+    @RequestMapping("/second.do")
+    public ModelAndView doSecond(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        logger.info("一个控制器多个访问路径second.do请求");
+        return new ModelAndView("/WEB-INF/Jsp/welcome.jsp");
+    }
+
+}
