d338087e3110d02db88ea89724c52712a60e30c2
JacKing <176288735@qq.com>
2017-08-23 18:28:39 +0800

    返回逻辑视图

--- SSM/ssm1/src/main/java/com/jack/king/fu/annotations/a10/annotationController.java
+++ SSM/ssm1/src/main/java/com/jack/king/fu/annotations/a10/annotationController.java
@@ -10,7 +10,7 @@ public class annotationController {
     public static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
     @RequestMapping("/pageto.do")
     public String doFirst()  {
-        logger.info("返回逻辑视图");
+        logger.info("返回逻 辑视图");
         return"page";
     }
 }
