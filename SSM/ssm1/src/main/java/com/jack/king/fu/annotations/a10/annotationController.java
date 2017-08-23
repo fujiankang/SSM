2b032e62123e95218393ed3a7dd39c7f0d4aa32b
JacKing <176288735@qq.com>
2017-08-23 18:24:12 +0800

    返回逻辑视图

---
+++ SSM/ssm1/src/main/java/com/jack/king/fu/annotations/a10/annotationController.java
@@ -0,0 +1,16 @@
+package com.jack.king.fu.annotations.a10;
+
+import org.apache.logging.log4j.LogManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class annotationController {
+    public static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+    @RequestMapping("/pageto.do")
+    public String doFirst()  {
+        logger.info("返回逻辑视图");
+        return"page";
+    }
+}
