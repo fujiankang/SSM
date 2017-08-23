daac36e8b3715b254ae4bd6feb6206bf4940764f
JacKing <176288735@qq.com>
2017-08-23 15:57:04 +0800

    整体接收，以对象的形式

---
+++ SSM/ssm1/src/main/java/com/jack/king/fu/annotations/a6/annotationController.java
@@ -0,0 +1,23 @@
+package com.jack.king.fu.annotations.a6;
+
+import com.jack.king.fu.vo.Student;
+import org.apache.logging.log4j.LogManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/test")
+public class annotationController {
+    public static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+
+    @RequestMapping("/register.do")
+    public ModelAndView doFirst(Student student)  {
+        logger.info("逐个参数的接收"+"姓名："+student.getName()+"年龄："+student.getAge());
+        ModelAndView mv=new ModelAndView();
+        mv.addObject("student",student);
+        mv.setViewName("/WEB-INF/Jsp/welcome.jsp");
+        return mv;
+    }
+}
--- SSM/ssm1/src/main/webapp/index.jsp
+++ SSM/ssm1/src/main/webapp/index.jsp
@@ -1,5 +1,12 @@
 <html>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <body>
 <h2>Hello World!</h2>
+<img src="${pageContext.request.contextPath}/images/zl.jpg">
+<form action="${pageContext.request.contextPath}/test/register.do">
+    姓名：<input type="text"  name="name" /><br>
+    年龄：<input type="text" name="age"/><br>
+    <input type="submit" value="注册">
+</form>
 </body>
 </html>
