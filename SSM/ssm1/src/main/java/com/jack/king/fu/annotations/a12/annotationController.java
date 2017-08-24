a0b5d326479c5278f98e0a7b74c3e7b0d7ff44ca
JacKing <176288735@qq.com>
2017-08-24 14:06:55 +0800

    返回逻辑视图

---
+++ SSM/ssm1/src/main/java/com/jack/king/fu/annotations/a12/annotationController.java
@@ -0,0 +1,30 @@
+package com.jack.king.fu.annotations.a12;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.logging.log4j.LogManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/test")
+public class annotationController {
+    public static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+    @RequestMapping("/ajax.do")
+    public void doAjax(String name, int age, HttpServletResponse rsp) throws IOException {
+        logger.info("Ajax请求到达======");
+        Map<String,Object> map=new HashMap<String, Object>();
+        map.put("name",name);
+        map.put("age",age);
+        String myJson= JSON.toJSONString(map,true);
+        //将数据返回浏览器
+        PrintWriter writer = rsp.getWriter();
+        writer.print(myJson);
+        writer.close();
+    }
+}
---
+++ SSM/ssm1/src/main/webapp/ajaxtest.jsp
@@ -0,0 +1,29 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Ajax请求页面</title>
+</head>
+<script type="text/javascript" src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+<script type="text/javascript">
+    $(
+        function () {
+            $("button").click(function () {
+                $.ajax({
+                    url:"test/ajax.do",
+                    data:{
+                        name:"zs",
+                        age:23
+                    },
+                    success:function (data) {
+                        var json=eval("("+data+")");
+                        alert(json.name+"      "+json.age);
+                    }
+                });
+            });
+        }
+    );
+</script>
+<body>
+<button>提交Ajax请求</button>
+</body>
+</html>
