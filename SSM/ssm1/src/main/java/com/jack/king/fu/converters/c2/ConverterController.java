5c42d495c6d63a107a7bb2d56e3526f8b35641f9
JacKing <176288735@qq.com>
2017-08-29 14:07:15 +0800

    使用正则表达式处理多种时间格式的问题

---
+++ SSM/ssm1/src/main/java/com/jack/king/fu/converters/c2/ConverterController.java
@@ -0,0 +1,25 @@
+package com.jack.king.fu.converters.c2;
+
+import org.apache.logging.log4j.LogManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/test")
+public class ConverterController {
+    public static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+    @RequestMapping("/cc2.do")
+    public ModelAndView ConverterCustom(int week, Date date)  {
+        logger.info("类型转换器,正则表达式解决多种格式时间问题");
+        logger.error("周数"+week);
+        logger.error("日期"+date);
+        ModelAndView mv=new ModelAndView();
+        mv.addObject("week",week);
+        mv.addObject("date",date);
+        mv.setViewName("/WEB-INF/Jsp/retc1.jsp");
+        return mv;
+    }
+}
